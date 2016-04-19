c0d64f6fa0f30e2df2d8b157584535efeaea9464
tvolkov <timofey.volkov@gmail.com>
2016-04-19 20:57:41 +0300

    cleanup

--- src/main/java/org/tvolkov/bulkdelete/controller/BulkDeleteController.java
+++ src/main/java/org/tvolkov/bulkdelete/controller/BulkDeleteController.java
@@ -7,11 +7,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.tvolkov.bulkdelete.exceptions.InternalServerError;
 import org.tvolkov.bulkdelete.service.BulkDeleteService;
 
-@EnableWebMvc
 @RequestMapping ("/bulk")
 @Controller
 public class BulkDeleteController {
